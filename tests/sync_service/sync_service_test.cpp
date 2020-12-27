14350c917e048deb3d5f8b5f244e040f14ae6ff8
Reutskiy Oleg <astlok@yandex.ru>
2020-12-27 17:18:57 +0300

    refactor

--- tests/sync_service/sync_service_test.cpp
+++ tests/sync_service/sync_service_test.cpp
@@ -18,27 +18,65 @@ using ::testing::SetArgReferee;
 
 class MockDatabase: public PostgressDB {
 public:
-    MOCK_METHOD(std::string, select, (const std::string str));
+    MOCK_METHOD(pqxx::result, select, (const std::string str));
+    MOCK_METHOD(void, insert_devise, (Message &message));
+    MOCK_METHOD(void, insert_file, (Message &message));
+    MOCK_METHOD(void, erase, (Message &message));
+    MOCK_METHOD(void, modified, (Message &message));
 };
 
-TEST(test_worker, test1) {
+TEST(test_update, test_login) {
     MockDatabase db;
     Message message;
     message.status = LOGIN;
-    message.user.user_name = "Oleg";
-    db.update(message);
+    message.user.devise.device_name = "Mac";
     std::string quote = "'";
-
     const std::string first_arg = "SELECT * from USERS_DEVISES "
                     "WHERE device_name = " + quote + message.user.devise.device_name + quote;
 
+
     EXPECT_CALL(db, select(first_arg))
-            .Times(1), Return("result");
+            .Times(1);
 
-    std::vector<Message> messages = db.update(message);
+    EXPECT_CALL(db, insert_devise(message))
+            .Times(1);
+}
 
-    ASSERT_EQ(messages[0].user.user_name, "Oleg");
-    ASSERT_EQ(messages[0].status, LOGIN);
+TEST(test_update, test_create) {
+    MockDatabase db;
+    Message message;
+    message.status = CREATE;
+    message.user.devise.device_name = "Mac";
 
+    EXPECT_CALL(db, select(_))
+            .Times(2);
 
+    EXPECT_CALL(db, insert_file(message))
+            .Times(1);
+}
+
+TEST(test_update, test_delete) {
+    MockDatabase db;
+    Message message;
+    message.status = DELETE;
+    message.user.devise.device_name = "Mac";
+
+    EXPECT_CALL(db, select(_))
+            .Times(1);
+
+    EXPECT_CALL(db, erase(message))
+            .Times(1);
+}
+
+TEST(test_update, test_modified) {
+    MockDatabase db;
+    Message message;
+    message.status = MODIFIED;
+    message.user.devise.device_name = "Mac";
+
+    EXPECT_CALL(db, select(_))
+            .Times(1);
+
+    EXPECT_CALL(db, modified(message))
+            .Times(1);
 }
